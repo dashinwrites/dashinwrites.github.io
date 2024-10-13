ca4eb18d5eeb7ddb24d292d6fa69f2685ac2ba09
Nihsad <nihsadd@gmail.com>
2024-10-13 15:06:52 -0600

    change chart colors...again

--- source/js/settings.js
+++ source/js/settings.js
@@ -4,4 +4,4 @@ const oldSheetID = `1XNHsAlHv62PHeXuyWfbegNueYi9QdeyvUPlJ0Qu64i0`;
 const successMessage = `<p class="fullWidth">Submission successful!</p>
 <button onclick="location.reload();" type="button" class="fullWidth submit">Back to form</button>`;
 const threadTags = ["vital", "priority", "rapidfire", "romantic", "family", "friends", "coworkers"];
-const chartColors = ['#A85555', '#D9995B', '#B2934E', '#E7C9A9', '#B56D45', '#C48661', '#A4765C', '#D6B89A'];
+const chartColors = ['#73626E', '#F5D8BC', '#F0B49E', '#F7E4BE', '#413E4A', '#8C7077', '#D3A79F', '#A69C9E'];
