f475ab79fd2fc159b5b5e936b323eede9a2fe5a0
dashinwrites <dashinwrites@gmail.com>
2026-01-26 13:45:59 -0700

    update styling

--- source/js/settings.js
+++ source/js/settings.js
@@ -1,7 +1,65 @@
-const deployID = 'AKfycbye5bJFoll2nVSSJGLll67l4hnXZxVlDW3xlApP0QBtTNrnZ-5hXkziWhcsFfMUTNSy';
-const sheetID = `1WtJG6Z0Ar1LtKDrgN1VbsIcMGkRDCiQ4oOHImPBla4Y`;
-const oldSheetID = `1XNHsAlHv62PHeXuyWfbegNueYi9QdeyvUPlJ0Qu64i0`;
+const deployID = 'AKfycbyU27BN16nzFbO0Vuboidv68ZpXXZ1vbNmR_vY25el_ijq3cliInm-6K5LC9McTbcrv';
+const sheetID = `1khon8SsEWHCd4QA9Vgal7RFNy_Y_yHtAzvzUIFVa6Ho`;
+const oldSheetID = `1KDKs6Kh7dXd9V3Vgcw9ipLPiaDsUqiyRPNGYw7wFnsQ`;
 const successMessage = `<p class="fullWidth">Submission successful!</p>
 <button onclick="location.reload();" type="button" class="fullWidth submit">Back to form</button>`;
 const threadTags = ["vital", "priority", "rapidfire", "romantic", "family", "friends", "coworkers"];
 const chartColors = ['#73626E', '#F5D8BC', '#F0B49E', '#F7E4BE', '#413E4A', '#8C7077', '#D3A79F', '#A69C9E'];
+
+const datasetOptions = {
+    backgroundColor: chartColors,
+    borderWidth: 5,
+    borderColor: '#3e3e3e'
+};
+
+const chartOptions = {
+    type: 'doughnut',
+    options: {
+        cutout: '35%',
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'bottom',
+                labels: {
+                    color: localStorage.getItem('theme') === 'light' ? '#767676' : '#e7e7e7',
+                    font: {
+                        family: 'Nunito Sans, sans-serif',
+                        size: '9',
+                        weight: 'bold'
+                    }
+                }
+            }
+        }
+    }
+};
+
+const noLegend = {
+    scales: {
+        x: {
+            ticks: {
+                color: localStorage.getItem('theme') === 'light' ? '#767676' : '#e7e7e7',
+                font: {
+                    family: 'Nunito Sans, sans-serif',
+                    size: '8',
+                    weight: 'bold'
+                }
+            }
+        },
+        y: {
+            ticks: {
+                color: localStorage.getItem('theme') === 'light' ? '#767676' : '#e7e7e7',
+                font: {
+                    family: 'Nunito Sans, sans-serif',
+                    size: '8',
+                    weight: 'bold'
+                }
+            }
+        },
+    },
+    responsive: true,
+    plugins: {
+        legend: {
+            display: false,
+        }
+    }
+};
