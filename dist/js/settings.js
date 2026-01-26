a336747427a4d4afa0533abd76322b70ad74374c
dashinwrites <dashinwrites@gmail.com>
2026-01-26 13:52:44 -0700

    fix settings

--- dist/js/settings.js
+++ dist/js/settings.js
@@ -1,6 +1,6 @@
-const deployID = 'AKfycbyU27BN16nzFbO0Vuboidv68ZpXXZ1vbNmR_vY25el_ijq3cliInm-6K5LC9McTbcrv';
-const sheetID = `1khon8SsEWHCd4QA9Vgal7RFNy_Y_yHtAzvzUIFVa6Ho`;
-const oldSheetID = `1KDKs6Kh7dXd9V3Vgcw9ipLPiaDsUqiyRPNGYw7wFnsQ`;
+const deployID = 'AKfycbyqMgf9ST73pRkrzfvwc5iBrJSedvHbn_0ESPKblzXVqagA680oyNrgKEpspKZHaclt';
+const sheetID = `1WtJG6Z0Ar1LtKDrgN1VbsIcMGkRDCiQ4oOHImPBla4Y`;
+const oldSheetID = `1XNHsAlHv62PHeXuyWfbegNueYi9QdeyvUPlJ0Qu64i0`;
 const successMessage = `<p class="fullWidth">Submission successful!</p>
 <button onclick="location.reload();" type="button" class="fullWidth submit">Back to form</button>`;
 const threadTags = ["vital", "priority", "rapidfire", "romantic", "family", "friends", "coworkers"];
